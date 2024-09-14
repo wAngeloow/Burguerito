17e6ec99c6b5466150ed7562d177e1ba342ab1b8
wAngeloow <angelodonada@gmail.com>
2024-09-14 14:19:48 -0300

    offiline deu errado

--- script.js
+++ script.js
@@ -12,18 +12,6 @@ const obsInput = document.getElementById("obs")
 
 let cart = [];
 
-// main.js ou o arquivo JavaScript principal do seu site
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      }).catch((error) => {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-    });
-  }
-  
-
 //BARRA DE ROLAGEM
 document.addEventListener('scroll', function() {
     const scrollBar = document.getElementById('scroll-bar');
